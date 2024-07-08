2689d0b3752ad1d688c6010a870ffd89ee43d35a
Toni <141995038+tonkugel@users.noreply.github.com>
2024-07-08 14:28:03 +0200

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,6 +1,15 @@
 const Game = require('../src/game').default
 const fs = require('fs')
 
+describe('App', () => {
+  it('Contains the compiled JavaScript', async (done) => {
+    fs.readFile('./public/main.js', 'utf8', (err, data) => {
+      expect(err).toBe(null)
+      done()
+    })
+  })
+})
+
 describe('Game', () => {
   let game, p1, p2
   beforeEach(() => {
